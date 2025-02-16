029989a3fae4901e35f2d2b971cc4cdb044d05be
TSilvaForte <tamara.sfok@gmail.com>
2025-02-15 23:57:59 -0300

    Adjusting responsive version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useAudio } from "../../context/musicContext";
 import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
